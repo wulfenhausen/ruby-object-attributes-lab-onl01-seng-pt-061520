3a6382fb46e5b0b713d7d441f2639b29038fe6e8
William Han Findiesen <william.findiesen@gmail.com>
2020-07-06 03:43:47 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+  def name =(persons_name)
+    @this_persons_name = persons_name
+  end
+  
+  def name
+    @this_persons_name
+  end
+  
+  def job =(job_name)
+    @job = job_name
+  end
+  
+  def job
+    @job
+  end
+    
+    
+end
