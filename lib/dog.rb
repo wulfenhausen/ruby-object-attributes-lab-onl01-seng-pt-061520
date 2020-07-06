aa4d75fe3c98c4bb9b8d9468b015304afe5ba0c9
William Han Findiesen <william.findiesen@gmail.com>
2020-07-06 03:45:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
   def name =(dogs_name)
-    @this_dogs_name = dogs_name
+    @name = dogs_name
   end
   
   def name
-    @this_dogs_name
+    @name
   end
   
   def breed =(breed_name)
